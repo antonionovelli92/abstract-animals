cc6cff128ac2e1128c59c82cfac102cd65b23637
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-11 12:37:27 +0200

    add passerotto

---
+++ src/java/jungle/kingdom/animals/Passerotto.java
@@ -0,0 +1,22 @@
+package java.jungle.kingdom.animals;
+
+import java.jungle.kingdom.Kingdom;
+
+public class Passerotto extends Kingdom {
+
+	  @Override
+	    public void dormi() {
+	        System.out.println("Zzz");
+	    }
+
+	    @Override
+	    public void verso() {
+	        System.out.println("Cip cip");
+	    }
+
+	    @Override
+	    public void mangia() {
+	        System.out.println("Semi e insetti");
+	    }
+    
+}
