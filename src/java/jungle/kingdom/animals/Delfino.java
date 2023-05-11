af5f6459a62def9b8e95db06543a38a151f95f93
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-11 12:41:33 +0200

    add delfino

---
+++ src/java/jungle/kingdom/animals/Delfino.java
@@ -0,0 +1,22 @@
+package java.jungle.kingdom.animals;
+
+import java.jungle.kingdom.Kingdom;
+
+public class Delfino extends Kingdom {
+	
+    @Override
+    public void dormi() {
+        System.out.println("ZzzDelfinozzZ");
+    }
+
+    @Override
+    public void verso() {
+        System.out.println("Fischi");
+    }
+
+    @Override
+    public void mangia() {
+    System.out.println("Pesce");
+	    }
+    
+}
